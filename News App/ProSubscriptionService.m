0e94390bdc4d7387b1a3fbea3438e8580055af0f
Andrew Christensen <andrew.christensen@markit.com>
2015-10-02 12:53:57 -0600

    Add store manager class.

---
+++ News App/ProSubscriptionService.m
@@ -0,0 +1,13 @@
+//
+//  ProSubscriptionService.m
+//  News App
+//
+//  Created by Andrew Christensen on 10/2/15.
+//  Copyright © 2015 Markit on Demand. All rights reserved.
+//
+
+#import "ProSubscriptionService.h"
+
+@implementation ProSubscriptionService
+
+@end
