6111ef288d1e19ee0be4d506bf87cedd36ef263b
Andrew Christensen <andrew.christensen@markit.com>
2015-10-02 10:04:25 -0600

    Initial Commit

---
+++ News App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News App
+//
+//  Created by Andrew Christensen on 10/2/15.
+//  Copyright © 2015 Markit on Demand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News App
+//
+//  Created by Andrew Christensen on 10/2/15.
+//  Copyright © 2015 Markit on Demand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
